e1a731c0725705484764c56860b91d937477028e
Anton Ukhanev <xedin.unknown@gmail.com>
2018-02-01 19:09:24 +0100

    Added `NormalizeWritableContainerCapableTrait`
    Fixes #7.

---
+++ src/NormalizeWritableContainerCapableTrait.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Dhii\Data\Container;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use ArrayAccess;
+use InvalidArgumentException;
+use Psr\Container\ContainerInterface;
+use stdClass;
+use Exception as RootException;
+
+/**
+ * Functionality for container normalization.
+ *
+ * @since [*next-version*]
+ */
+trait NormalizeWritableContainerCapableTrait
+{
+    /**
+     * Normalizes a container.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|ArrayAccess|stdClass $container The container to normalize.
+     *
+     * @throws InvalidArgumentException If the container is invalid.
+     *
+     * @return array|ArrayAccess|stdClass A container that can be written to.
+     */
+    protected function _normalizeWritableContainer($container)
+    {
+        $container = $this->_normalizeContainer($container);
+
+        if ($container instanceof ContainerInterface) {
+            throw $this->_createInvalidArgumentException(
+                $this->__('Invalid container'),
+                null,
+                null,
+                $container
+            );
+        }
+
+        return $container;
+    }
+
+    /**
+     * Normalizes a container.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|ArrayAccess|stdClass|ContainerInterface $container The container to normalize.
+     *
+     * @throws InvalidArgumentException If the container is invalid.
+     *
+     * @return array|ArrayAccess|stdClass|ContainerInterface Something that can be used with
+     *                                                       {@see ContainerGetCapableTrait#_containerGet()} or
+     *                                                       {@see ContainerHasCapableTrait#_containerHas()}.
+     */
+    abstract protected function _normalizeContainer($container);
+
+    /**
+     * Creates a new invalid argument exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $message  The error message, if any.
+     * @param int|null               $code     The error code, if any.
+     * @param RootException|null     $previous The inner exception for chaining, if any.
+     * @param mixed|null             $argument The invalid argument, if any.
+     *
+     * @return InvalidArgumentException The new exception.
+     */
+    abstract protected function _createInvalidArgumentException(
+        $message = null,
+        $code = null,
+        RootException $previous = null,
+        $argument = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see sprintf()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
+}
