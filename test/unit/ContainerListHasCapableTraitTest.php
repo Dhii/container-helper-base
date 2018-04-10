cdb10faf8d3b2714696a6f8d3dd2cc955e650b22
Anton Ukhanev <xedin.unknown@gmail.com>
2018-04-10 15:11:33 +0200

    Added unit test for `ContainerListHasCapableTrait`
    Addresses #24.

---
+++ test/unit/ContainerListHasCapableTraitTest.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace Dhii\Data\Container\UnitTest;
+
+use Dhii\Data\Container\ContainerListHasCapableTrait as TestSubject;
+use Psr\Container\ContainerInterface;
+use Xpmock\TestCase;
+use Exception as RootException;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use PHPUnit_Framework_MockObject_MockBuilder as MockBuilder;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class ContainerListHasCapableTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'Dhii\Data\Container\ContainerListHasCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $methods The methods to mock.
+     *
+     * @return MockObject The new instance.
+     */
+    public function createInstance($methods = [])
+    {
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+            '__',
+        ]);
+
+        $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+            ->setMethods($methods)
+            ->getMockForTrait();
+
+        $mock->method('__')
+                ->will($this->returnArgument(0));
+
+        return $mock;
+    }
+
+    /**
+     * Merges the values of two arrays.
+     *
+     * The resulting product will be a numeric array where the values of both inputs are present, without duplicates.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $destination The base array.
+     * @param array $source      The array with more keys.
+     *
+     * @return array The array which contains unique values
+     */
+    public function mergeValues($destination, $source)
+    {
+        return array_keys(array_merge(array_flip($destination), array_flip($source)));
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * This is particularly useful for cases where the mock is based on an
+     * internal class, such as in the case with exceptions. Helps to avoid
+     * writing hard-coded stubs.
+     *
+     * @since [*next-version*]
+     *
+     * @param string   $className      Name of the class for the mock to extend.
+     * @param string[] $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return MockBuilder The builder for a mock of an object that extends and implements
+     *                     the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf('abstract class %1$s extends %2$s implements %3$s {}', [
+            $paddingClassName,
+            $className,
+            implode(', ', $interfaceNames),
+        ]);
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName);
+    }
+
+    /**
+     * Creates a mock that uses traits.
+     *
+     * This is particularly useful for testing integration between multiple traits.
+     *
+     * @since [*next-version*]
+     *
+     * @param string[] $traitNames Names of the traits for the mock to use.
+     *
+     * @return MockBuilder The builder for a mock of an object that uses the traits.
+     */
+    public function mockTraits($traitNames = [])
+    {
+        $paddingClassName = uniqid('Traits');
+        $definition = vsprintf('abstract class %1$s {%2$s}', [
+            $paddingClassName,
+            implode(
+                ' ',
+                array_map(
+                    function ($v) {
+                        return vsprintf('use %1$s;', [$v]);
+                    },
+                    $traitNames)),
+        ]);
+        var_dump($definition);
+        eval($definition);
+
+        return $this->getMockBuilder($paddingClassName);
+    }
+
+    /**
+     * Creates a new exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The exception message.
+     *
+     * @return RootException|MockObject The new exception.
+     */
+    public function createException($message = '')
+    {
+        $mock = $this->getMockBuilder('Exception')
+            ->setConstructorArgs([$message])
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Creates a new container.
+     *
+     * @since [*next-version*]
+     *
+     * @param array|null $methods The methods to mock.
+     *
+     * @return MockObject|ContainerInterface The new containers.
+     */
+    public function createContainer($methods = [])
+    {
+        is_array($methods) && $methods = $this->mergeValues($methods, [
+            '__',
+        ]);
+
+        $mock = $this->getMockBuilder('Psr\Container\ContainerInterface')
+            ->setMethods($methods)
+            ->getMock();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'A valid instance of the test subject could not be created.'
+        );
+    }
+
+    /**
+     * Tests whether `_containerListHas()` works as expected when the list has the key.
+     *
+     * @since [*next-version*]
+     */
+    public function testContainerListHasTrue()
+    {
+        $key = uniqid('key');
+        $container1 = $this->createContainer(['has', 'get']);
+        $container2 = $this->createContainer(['has', 'get']);
+        $containerList = [$container1, $container2];
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(1))
+            ->method('_normalizeIterable')
+            ->with($containerList)
+            ->will($this->returnArgument(0));
+        $subject->expects($this->exactly(count($containerList)))
+            ->method('_containerHas')
+            ->will($this->returnValueMap([
+                [$container1, $key, false],
+                [$container2, $key, true],
+            ]));
+
+        $result = $_subject->_containerListHas($key, $containerList);
+        $this->assertTrue($result, 'Container list wrongly reported to not have the key');
+    }
+
+    /**
+     * Tests whether `_containerListHas()` works as expected when the list does not have the key.
+     *
+     * @since [*next-version*]
+     */
+    public function testContainerListHasFalse()
+    {
+        $key = uniqid('key');
+        $container1 = $this->createContainer(['has', 'get']);
+        $container2 = $this->createContainer(['has', 'get']);
+        $containerList = [$container1, $container2];
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(1))
+            ->method('_normalizeIterable')
+            ->with($containerList)
+            ->will($this->returnArgument(0));
+        $subject->expects($this->exactly(count($containerList)))
+            ->method('_containerHas')
+            ->will($this->returnValueMap([
+                [$container1, $key, false],
+                [$container2, $key, false],
+            ]));
+
+        $result = $_subject->_containerListHas($key, $containerList);
+        $this->assertFalse($result, 'Container list wrongly reported to not have the key');
+    }
+}
